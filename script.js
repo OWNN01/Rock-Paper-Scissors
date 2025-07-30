8884f1da9e9979d1ff1cc5b9a089da9a0398ae4a
Ownn01 <116043201+OWNN01@users.noreply.github.com>
2025-07-29 19:56:18 -0500

    Add the playgame criteria

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function getComputerChoice() {
     let computerChoice = "";
 
     if (choiceValue < 0.33) {
-        computerChoice = "Rock"
+        computerChoice = "Rock";
     } else if (choiceValue >= .33 && choiceValue < .66) {
-        computerChoice = "Paper"
+        computerChoice = "Paper";
     } else {
-        computerChoice = "Scissors"
+        computerChoice = "Scissors";
     }
 
     return computerChoice;
@@ -41,4 +41,50 @@ function getHumanChoice() {
 
 
 
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+
+    if ((humanChoice === "Rock") && (computerChoice === "Scissors")) {
+        return 1;
+    } else if ((humanChoice === "Paper") && (computerChoice === "Rock")) {
+        return 1;
+    } else if ((humanChoice === "Scissors") && (computerChoice === "Paper")) {
+        return 1;
+    } else if (humanChoice === computerChoice) {
+        console.log("Tie!")
+    } else {
+        return 2;
+    }
+
+}
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    while ((humanScore < 5) || (computerScore < 5)) {
+        let outcome = 0;
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        outcome = playRound(humanChoice, computerChoice);
+        if (outcome === 1) {
+            console.log(humanChoice + " beats " + computerChoice + "!" + " Human wins this round!");
+            humanScore += 1;
+        } else if (outcome === 2) {
+            console.log(computerChoice + " beats " + humanChoice + "!" + " Computer wins this round!");
+            computerScore += 1;
+        }
+        console.log("Human Score: " + humanScore);
+        console.log("ComputerScore: " + computerScore);
+    }
+
+    if (humanScore === 5) {
+        console.log("Human wins!");
+    } else {
+        console.log("Computer wins!");
+    }
+
+}
+
+playGame();
