4dc70cb548be6f2a384a60afe44e321288391772
Ownn01 <116043201+OWNN01@users.noreply.github.com>
2025-08-28 20:21:04 -0500

    Clean up the code and add in the dynamic score counter

--- script.js
+++ script.js
@@ -1,68 +1,28 @@
-// Function to play through the game of Rock Paper Scissors by keeping score up to 5. 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+
+// Score variables
+let hScore = 0;
+let cScore = 0;
 
 
-    while ((humanScore < 1) && (computerScore < 1)) {
-        let outcome = 0;
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        outcome = playRound(humanChoice, computerChoice);
-        if (outcome === 1) {
-            console.log(humanChoice + " beats " + computerChoice + "!" + " Human wins this round!");
-            humanScore += 1;
-        } else if (outcome === 2) {
-            console.log(computerChoice + " beats " + humanChoice + "!" + " Computer wins this round!");
-            computerScore += 1;
-        }
-        console.log("Human Score: " + humanScore);
-        console.log("ComputerScore: " + computerScore);
-    }
+//Function to get a random choice by converting math.random values into choices.
 
-    if (humanScore === 5) {
-        console.log("Human wins!");
+function getComputerChoice() {
+    let choiceValue = Math.random();
+    let computerChoice = "";
+
+    if (choiceValue < 0.33) {
+        computerChoice = "Rock";
+    } else if (choiceValue >= .33 && choiceValue < .66) {
+        computerChoice = "Paper";
     } else {
-        console.log("Computer wins!");
+        computerChoice = "Scissors";
     }
 
-
+    return computerChoice;
 }
 
-const btnRock = document.querySelector(".Rock");
-const btnPaper = document.querySelector(".Paper");
-const btnScissors = document.querySelector(".Scissors");
+// Function to play one round and return text output representing who won.
 
-btnRock.addEventListener("click", (e) => {
-    console.log(playRound(e.target.innerText, getComputerChoice()));
-});
-
-btnPaper.addEventListener("click", (e) => {
-    console.log(playRound(e.target.innerText, getComputerChoice()));
-});
-
-btnScissors.addEventListener("click", (e) => {
-    console.log(playRound(e.target.innerText, getComputerChoice()));
-});
-
-const score = document.querySelector(".Score");
-const humanScore = document.createElement("p");
-humanScore.classList.add("humanScore");
-humanScore.textContent = "I'm the human score."
-humanScore.style.cssText = "Color: blue;"
-
-const computerScore = document.createElement("p");
-computerScore.classList.add("computerScore");
-computerScore.textContent = "I'm the computer score."
-computerScore.style.cssText = "Color: red;"
-
-score.appendChild(humanScore);
-score.appendChild(computerScore);
-
-
-// Function to play one round and return a numerical output representing who won.
-// 1 for the human, 2 for the computer, and nothing for a tie.
-// There are 3 win cases, 1 tie case, and one loss case for the user.
 function playRound(humanChoice, computerChoice) {
 
     if ((humanChoice === "Rock") && (computerChoice === "Scissors")) {
@@ -79,37 +39,87 @@ function playRound(humanChoice, computerChoice) {
 
 }
 
-//Function to get a random choice by converting math.random values into choices.
-function getComputerChoice() {
-    let choiceValue = Math.random();
-    let computerChoice = "";
+// Get button DOM elements.
 
-    if (choiceValue < 0.33) {
-        computerChoice = "Rock";
-    } else if (choiceValue >= .33 && choiceValue < .66) {
-        computerChoice = "Paper";
+const btnRock = document.querySelector(".Rock");
+const btnPaper = document.querySelector(".Paper");
+const btnScissors = document.querySelector(".Scissors");
+
+// Add the button event listeners.
+
+btnRock.addEventListener("click", (e) => {
+    let humanChoice = e.target.innerText;
+    let computerChoice = getComputerChoice();
+    let outcome = playRound(e.target.innerText, computerChoice);
+    updateScore(outcome, humanChoice, computerChoice);
+});
+
+btnPaper.addEventListener("click", (e) => {
+    let humanChoice = e.target.innerText;
+    let computerChoice = getComputerChoice();
+    let outcome = playRound(e.target.innerText, computerChoice);
+    updateScore(outcome, humanChoice, computerChoice);
+});
+
+btnScissors.addEventListener("click", (e) => {
+    let humanChoice = e.target.innerText;
+    let computerChoice = getComputerChoice();
+    let outcome = playRound(e.target.innerText, computerChoice);
+    updateScore(outcome, humanChoice, computerChoice);
+});
+
+// Set the dynamic score DOM elements.
+
+const score = document.querySelector(".Score");
+const humanScore = document.createElement("p");
+humanScore.classList.add("humanScore");
+humanScore.textContent = "I'm the human score: " + hScore;
+humanScore.style.cssText = "Color: blue;"
+
+const computerScore = document.createElement("p");
+computerScore.classList.add("computerScore");
+computerScore.textContent = "I'm the computer score: " + cScore;
+computerScore.style.cssText = "Color: red;"
+
+const roundOutcome = document.createElement("p");
+roundOutcome.classList.add("roundOutcome");
+
+// Update the score and report the outcome of the round.
+
+function updateScore(outcome, humanChoice, computerChoice) {
+
+    if (outcome === "Human Wins") {
+        roundOutcome.textContent = humanChoice + " beats " + computerChoice + "!" + " Human wins this round!";
+        hScore += 1;
+    } else if (outcome === "Computer Wins") {
+        roundOutcome.textContent = computerChoice + " beats " + humanChoice + "!" + " Computer wins this round!";
+        cScore += 1;
     } else {
-        computerChoice = "Scissors";
+        roundOutcome.textContent = computerChoice + " ties with " + humanChoice + "!" + " Nobody wins this round!";
     }
 
-    return computerChoice;
+    if (hScore === 5) {
+        roundOutcome.textContent = "Human wins!";
+        hScore = 0;
+        cScore = 0;
+        score.removeChild(humanScore);
+        score.removeChild(computerScore);
+    } else if (cScore === 5) {
+        roundOutcome.textContent = "Computer wins!";
+        hScore = 0;
+        cScore = 0;
+        score.removeChild(humanScore);
+        score.removeChild(computerScore);
+    }
+
+    score.appendChild(roundOutcome);
+    score.appendChild(humanScore);
+    score.appendChild(computerScore);
+
+    humanScore.textContent = "I'm the human score: " + hScore;
+    computerScore.textContent = "I'm the computer score: " + cScore;
+
 }
 
 
-// Function to prompt the user for a choice.
-function getHumanChoice() {
-    let rightChoice = true;
-    let humanChoice = '';
 
-    while (rightChoice) {
-
-        humanChoice = prompt("Type Rock, Paper, or Scissors. (Exactly)");
-
-        if (humanChoice === "Rock" || humanChoice === "Paper" || humanChoice === "Scissors") {
-            rightChoice = false;
-            return humanChoice;
-        } else {
-            console.log("Please type one of the choices. CASE SENSITIVE.");
-        }
-    }
-}
